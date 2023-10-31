9ead7d433c30eb887ce282e2e007060b956cd037
chanona <122000806+DazhZs@users.noreply.github.com>
2023-10-30 20:12:57 -0600

    se reestrucutra las carpetas

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import AdminDashboard from "./views/admin/AdminDashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <AdminDashboard /> {}
+    </div>
+  );
+}
+
+export default App;
