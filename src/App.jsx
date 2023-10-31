cbd090388758b5107392f9f40da7c253dd593748
chanona <122000806+DazhZs@users.noreply.github.com>
2023-10-30 21:24:20 -0600

    se reesctructura el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import AdminDashboard from "./views/admin/AdminDashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <AdminDashboard /> {/* Renderiza el componente AdminDashboard aquí */}
+    </div>
+  );
+}
+
+export default App;
