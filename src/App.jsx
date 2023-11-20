1f38e2daefba710bb3d9f992603ea70f589981f4
chanona <122000806+DazhZs@users.noreply.github.com>
2023-11-20 16:04:57 -0600

    corregir archivos y añadir componente Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
-import HomePage from './views/client/homePage';
+import HomePage from './views/client/HomePage';
 import AdminDashboard from './views/admin/AdminDashboard';
 import PayPage from './views/client/PayPage';
 import Navbar from './views/client/components/Navbar';
@@ -8,6 +8,8 @@ import Sidebar from './views/client/components/Sidebar';
 import CategoryPage3 from './views/client/CategoryPage3';
 import CategoryPage2 from './views/client/CategoryPage2';
 import CategoryPage1 from './views/client/CategoryPage1';
+import Footer from './views/client/components/Footer';
+
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -28,6 +30,7 @@ function App() {
           <Route path="/pay" element={<PayPage />} />
           <Route path="/" element={<HomePage />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
--- src/views/client/HomePage.jsx
+++ src/views/client/HomePage.jsx
@@ -11,6 +11,8 @@ function HomePage() {
         { name: "Papas", price: 1000, imageUrl: "https://images.unsplash.com/photo-1541592391523-5ae8c2c88d10?auto=format&fit=crop&q=80&w=1170&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
         { name: "Bonless", price: 1000, imageUrl: "https://images.unsplash.com/photo-1476224203421-9ac39bcb3327?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
 
+
+
     ]
     return (
         <div className="h-screen w-full flex-grow">
